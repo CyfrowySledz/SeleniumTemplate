442c44601842656845b36d235a6249aee9279a87
CyfrowySledz <digitalherrings@gmail.com>
2018-05-13 19:15:54 +0200

    Initial commit

---
+++ src/main/java/org/seleniumframe/config/DriverSetup.java
@@ -0,0 +1,11 @@
+package org.seleniumframe.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public interface DriverSetup {
+
+    WebDriver getWebDriverObject(DesiredCapabilities desiredCapabilities);
+
+    DesiredCapabilities getDesiredCapabilities();
+}
